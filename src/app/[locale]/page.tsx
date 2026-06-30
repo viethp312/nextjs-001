6cba152b38cc551ac8e569d8a5c33a2ea4766741
viethp312 <viethp312@gmail.com>
2026-06-30 09:49:51 +0700

    feat: init i18n

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,22 @@
+import { type Locale, useTranslations } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
+import { use } from "react";
+
+import { routing } from "@/i18n/routing";
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+type Props = {
+  params: Promise<{ locale: Locale }>;
+};
+
+export default function Home({ params }: Props) {
+  const { locale } = use(params);
+  const t = useTranslations("HomePage");
+
+  setRequestLocale(locale);
+
+  return <div>{t("title")}</div>;
+}
